81b8cf0d9987b7d357f1c1d22aab27edec5ea805
yabag <yabagratyan@mail.ru>
2022-06-04 08:04:22 +0300

    changed test.py

--- Selfedu/test.py
+++ Selfedu/test.py
@@ -1,16 +1,13 @@
-class MediaPlayer:
+class Graph:
+    def set_data(self, data):
+        self.data = data
 
-    def open(self, file):
-        self.filename = file
+    LIMIT_Y = [0, 10]
 
-    def play(self):
-        print("Воспроизведение {}".format(self.filename))
+    def draw(self):
+        return print(*self.data)
 
 
-media1 = MediaPlayer()
-media2 = MediaPlayer()
-
-media1.open("filemedia1")
-media2.open("filemedia2")
-media1.play()
-media2.play()
+graph1 = Graph()
+graph1.set_data([10, -5, 100, 20, 0, 80, 45, 2, 5, 7])
+graph1.draw()
