1f47d9748dbffa5d44df5bf162e71c7668d4f4bd
yabag <yabagratyan@mail.ru>
2022-06-14 17:36:07 +0300

    chenged ex14_parsing_files.py

--- Exercises/ex14_parsing_files.py
+++ Exercises/ex14_parsing_files.py
@@ -1,16 +1,31 @@
-class Parsing:
-    def __init__(self, file_1, file_2):
-        self.file_1 = file_1
-        self.file_2 = file_2
+ru_txt = open("ru.txt")
+en_txt = open("en.txt")
 
-    def __str__(self):
-        return "В файле '{}' нет следующих строк:\n".format(self.file_2)
+s_ru = ru_txt.readlines()
+s_en = en_txt.readlines()
+new_ru = []
+new_en = []
 
-    def parsing_files(self, file_1, file_2):
-        pass
+for line in s_ru:
+    if "=" in line:
+        x = line.split(' = ')
+        new_ru.append(x[0])
+
+for line in s_en:
+    if "=" in line:
+        x = line.split(' = ')
+        new_en.append(x[0])
+
+missed_str = []
+for i in range(len(new_ru)):
+    if new_ru[i] not in new_en:
+        missed_str.append(new_ru[i])
 
 
-if __name__ == "__main__":
-    p = Parsing('ru.txt', 'en.txt')
-    print(p)
-    # parsing_files()
+print("В файле en.txt нет следующих строк:")
+for i, elem in enumerate(missed_str):
+    print(f'{i + 1}. {elem}')
+
+
+ru_txt.close()
+en_txt.close()
