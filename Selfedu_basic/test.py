b93e956e4ce8a91f5b27317e3dd4cab9d2c3678f
yabag <yabagratyan@mail.ru>
2022-07-03 20:05:51 +0300

    changed test.py

--- Selfedu_basic/test.py
+++ Selfedu_basic/test.py
@@ -1,5 +1,13 @@
-lst = input().split()
-d = dict([[x for x in s.split('=')] for s in lst])
+import shlex
+import sys
 
-print(*sorted(d.items()))
-# print(d)
+# считывание списка из входного потока
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+# здесь продолжайте программу (используйте список lst_in)
+
+lst = [s.split() for s in lst_in]
+print(lst)
+d = {k: [] for k in [lst[i][1] for i in range(len(lst))]}
+for k,v in lst:
+
+print(d)
