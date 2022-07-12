46e8d9dcf4e0c7fc5acef91c284895a7e69c3554
yabag <yabagratyan@mail.ru>
2022-07-12 11:35:12 +0300

    done final in dict

--- Selfedu_basic/test.py
+++ Selfedu_basic/test.py
@@ -1,11 +1,19 @@
-import sys
+things = {'карандаш': 20, 'зеркальце': 100, 'зонт': 500, 'рубашка': 300,
+          'брюки': 1000, 'бумага': 200, 'молоток': 600, 'пила': 400, 'удочка': 1200,
+          'расческа': 40, 'котелок': 820, 'палатка': 5240, 'брезент': 2130, 'спички': 10}
 
-lst_in = list(map(str.strip, sys.stdin.readlines()))
-d = dict.fromkeys(lst_in)
+N = float(input()) * 1000
 
-d_items = dict([[k, v] for k, v in [s.split() for s in lst_in]])
+my_lst = [(k, v) for k, v in things.items()]
+my_lst.sort(key=lambda x: x[1], reverse=True)
 
-for item in d_items:
-    d.setdefault()
+bag = []
 
-print(d)
+for thing in my_lst:
+    if thing[1] <= N:
+        bag.append(thing[0])
+        N -= thing[1]
+    else:
+        continue
+
+print(*bag)
