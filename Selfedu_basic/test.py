d89dfbcc2c6048c93078c38810659cf220754d51
yabag <yabagratyan@mail.ru>
2022-07-19 12:45:03 +0300

    done func get_nod

--- Selfedu_basic/test.py
+++ Selfedu_basic/test.py
@@ -1,9 +1,51 @@
-def over_six(s):
-    return len(s) > 5
+import time
 
 
-cities = input().split()
+def get_nod(a, b):
+    """Вычисляется НОД для натуральных чисел a и b
+    по алгоритму Евклида.
+    :param a: первое натуральное число
+    :param b: второе натуральное число
+    :return: НОД
+    """
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
 
-lst = [city for city in cities if over_six(city)]
+    return a
 
-print(*lst)
+def test_nod(func):
+    # --- тест №1 ---------------
+    a = 28
+    b = 35
+    res = func(a, b)
+    if res == 7:
+        print("#tets1 - ok")
+    else:
+        print("#test1 - fail")
+
+    # --- тест №2 ---------------
+    a = 100
+    b = 1
+    res = func(a, b)
+    if res == 1:
+        print("#tets2 - ok")
+    else:
+        print("#test2 - fail")
+
+    # --- тест №3 ---------------
+    a = 2
+    b = 100000000
+    st = time.time()
+    res = func(a, b)
+    et = time.time()
+    dt = et - st
+    if res == 2 and dt < 1:
+        print("#tets3 - ok")
+    else:
+        print("#test3 - fail")
+
+
+test_nod(get_nod)
