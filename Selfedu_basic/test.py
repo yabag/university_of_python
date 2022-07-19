bd4c77f5c38573c33e1af0bad999a91dcbf00218
yabag <yabagratyan@mail.ru>
2022-07-19 12:51:11 +0300

    done FAST get_nod

--- Selfedu_basic/test.py
+++ Selfedu_basic/test.py
@@ -3,16 +3,17 @@ import time
 
 def get_nod(a, b):
     """Вычисляется НОД для натуральных чисел a и b
-    по алгоритму Евклида.
+    по быстрому алгоритму Евклида.
     :param a: первое натуральное число
     :param b: второе натуральное число
     :return: НОД
     """
-    while a != b:
-        if a > b:
-            a -= b
-        else:
-            b -= a
+
+    if a < b:
+        a, b = b, a
+
+    while b != 0:
+        a, b = b, a % b
 
     return a
 
@@ -37,7 +38,7 @@ def test_nod(func):
 
     # --- тест №3 ---------------
     a = 2
-    b = 100000000
+    b = 10000000000000
     st = time.time()
     res = func(a, b)
     et = time.time()
