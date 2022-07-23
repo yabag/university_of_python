bd6a443dd358df43d66947996a4aafb550f63668
yabag <yabagratyan@mail.ru>
2022-07-23 07:58:20 +0300

    recursion

--- Selfedu_basic/test.py
+++ Selfedu_basic/test.py
@@ -1,16 +1,2 @@
-def is_isolate(a, b, c, d):
-    if a+b+c+d > 1:
-        return False
-    else:
-        return True
-
-
-def verify(lst):
-    flag = True
-    for i in range(len(lst) - 1):
-        for j in range(len(lst) - 1):
-            if lst[i][j] == 1:
-                flag = is_isolate(lst[i][j], lst[i][j+1], lst[i+1][j], lst[i+1][j+1])
-                if not flag:
-                    return flag
-    return flag
+def get_rec_N(N):
+    pass
