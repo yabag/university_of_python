b97a92417dfa3f8726d4324cd16f3b6a6d7b9c06
yabag <yabagratyan@mail.ru>
2022-07-26 19:46:33 +0300

    some changeses

--- Selfedu_basic/test.py
+++ Selfedu_basic/test.py
@@ -1,18 +1,4 @@
-def filter_lst(it, key=None):
-    if key is None:
-        return tuple(it)
+def create_global(x):
+    global TOTAL
+    TOTAL = x
 
-    res = ()
-    for x in it:
-        if key(x):
-            res += (x,)
-
-    return res
-
-
-lst = list(map(int, input().split()))
-
-print(*filter_lst(lst))
-print(*filter_lst(lst, lambda x: x < 0))
-print(*filter_lst(lst, lambda x: x >= 0))
-print(*filter_lst(lst, lambda x: 2 < x < 6))
